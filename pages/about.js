b4d7a820a7e58fe54ebc59e305a1e6670c43fc4f
Emre TINAZ <emre.tinaz@trivago.com>
2021-04-06 15:29:15 +0200

    initial push :)

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <Head>
+            <title>Ninja List | About </title>
+            <meta name="keywords" content="ninja" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam est non sit quas. Eos reprehenderit culpa aperiam nostrum quidem, earum corrupti, veniam sit velit nam quas repudiandae vitae aliquid est.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam est non sit quas. Eos reprehenderit culpa aperiam nostrum quidem, earum corrupti, veniam sit velit nam quas repudiandae vitae aliquid est.</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
